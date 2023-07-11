17de99f70a01dc9238e675fecd89c945e584681d
RKouchoo <r.kouchoo1@gmail.com>
2023-07-11 14:08:37 +1000

    tle mre

--- src/com/rkouchoo/apts/tlehandler.java
+++ src/com/rkouchoo/apts/tlehandler.java
@@ -1,12 +1,24 @@
 package com.rkouchoo.apts;
 
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
 public class tlehandler {
 
 	private tleinterpreter interpreter;
+
 	
+	private void GetLatestTLE(String tleDownload) {
+		
+		URL tleWebsite = new URL(tleDownload);
+		ReadableByteChannel rbc = Channels.newChannel(tleWebsite.openStream());
+		FileOutputStream fos = new FileOutputStream("information.html");
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		
+		
+	}
 	
-	
-	
-	
-	
+
 }
